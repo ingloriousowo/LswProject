04b9de9acbb8c23c6ac3f7c3bba9dbfd88903352
ingloriousOwO <jorge.villalobos2002@yahoo.com>
2022-11-02 16:46:01 -0300

    arreglos minimos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Componente } from './interfaces/interfaces';
 import { DataService } from './services/data.service';
+import { InAppBrowser } from '@awesome-cordova-plugins/in-app-browser/ngx';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
@@ -12,9 +14,34 @@ export class AppComponent {
 
   componentes: Observable<Componente[]>;
   
-  constructor(private dataService: DataService) {}
+  constructor(private dataService: DataService, private iab: InAppBrowser, private alert: AlertController) {}
 
   ngOnInit(){
     this.componentes = this.dataService.getOpcionesMenu();
+  }
+
+  async clickGithub(){
+    const alerta = await this.alert.create({
+      header: 'Estar por Salir de la App',
+      buttons: [
+        {
+          text:'Cancelar',
+          role: 'cancel'
+        },
+        {
+          text:'Confirmar',
+          handler: () =>{
+            this.abrirGithub();
+          }
+        }
+      ]
+    });
+
+    await alerta.present();
+  }
+
+  abrirGithub(){
+    const browser = this.iab.create('https://github.com/ingloriousowo/LswProject.git');
+    browser.show();
   }
 }
